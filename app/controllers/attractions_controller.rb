f80cf748b90a96838e97d726aaaaee193a19118a
daicoden <daicoden@copypastel.com>
2009-04-22 08:34:25 -0400

    Attraction and Comments are now resources, need to spice up the view a little.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,2 +1,85 @@
 class AttractionsController < ApplicationController
+  # GET /attractions
+  # GET /attractions.xml
+  def index
+    @attractions = Attraction.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @attractions }
+    end
+  end
+
+  # GET /attractions/1
+  # GET /attractions/1.xml
+  def show
+    @attraction = Attraction.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @attraction }
+    end
+  end
+
+  # GET /attractions/new
+  # GET /attractions/new.xml
+  def new
+    @attraction = Attraction.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @attraction }
+    end
+  end
+
+  # GET /attractions/1/edit
+  def edit
+    @attraction = Attraction.find(params[:id])
+  end
+
+  # POST /attractions
+  # POST /attractions.xml
+  def create
+    @attraction = Attraction.new(params[:attraction])
+
+    respond_to do |format|
+      if @attraction.save
+        flash[:notice] = 'Attraction was successfully created.'
+        format.html { redirect_to(@attraction) }
+        format.xml  { render :xml => @attraction, :status => :created, :location => @attraction }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @attraction.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /attractions/1
+  # PUT /attractions/1.xml
+  def update
+    @attraction = Attraction.find(params[:id])
+
+    respond_to do |format|
+      if @attraction.update_attributes(params[:attraction])
+        flash[:notice] = 'Attraction was successfully updated.'
+        format.html { redirect_to(@attraction) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @attraction.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /attractions/1
+  # DELETE /attractions/1.xml
+  def destroy
+    @attraction = Attraction.find(params[:id])
+    @attraction.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(attractions_url) }
+      format.xml  { head :ok }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :comments
+
+  map.resources :attractions
+
+  map.resources :comments
+
   map.resources :attractions
 
   # The priority is based upon order of creation: first created -> highest priority.
